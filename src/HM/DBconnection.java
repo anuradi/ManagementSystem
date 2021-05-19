758d48b1af32f34eff45a900e37a73f76cfeaada
Nisal <iamnisal123@gmail.com>
2021-05-19 12:17:03 +0530

    change pakage name

---
+++ src/HM/DBconnection.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HM;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Nisal
+ */
+
+public class DBconnection {
+    	private static Connection con;
+        private static Statement stmt;
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/management_system","root", "");
+			stmt = con.createStatement();
+			
+		} catch (Exception e) {
+			 System.out.println("Databace connection is unsuccess");
+		}
+                return con;
+	}
+        
+        
+        
+}
